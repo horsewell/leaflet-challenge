9f9b6781014415dbdd44c0fb0ddd864644dcbf16
Tyson <tyson@horsewell.me>
2024-03-09 17:16:48 +0800

    Update logic.js
    * load the data

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,3 +1,13 @@
+// debug
+var bDebug = true;
+
 // URL of the API endpoint to query
 const USGS_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+if (bDebug) { console.log(USGS_URL); }
+
+d3.json(USGS_URL).then((data) => {
+    // test that the data loads
+    if (bDebug) { console.log(data.features); }
+});
 
